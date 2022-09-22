435168681cb21551c96265f356142dfa9665e446
Mikhail Kuzmin <mikhalkuzmin@yandex.ru>
2022-09-22 12:53:05 +0400

    Task2

--- Program.cs
+++ Program.cs
@@ -1,18 +1,45 @@
 ﻿using System;
 
-Console.WriteLine("Введите число 1");
-int a= int.Parse(Console.ReadLine());
-Console.WriteLine("Введите число 2");
-int b= int.Parse(Console.ReadLine());
-if (a > b)
-{
-    Console.WriteLine("max " + a);
-}
-else if (b > a)
-{
-    Console.WriteLine("max " + b);
-}
-else
-{
-    Console.WriteLine("Значения ровны");
-}
+// Console.WriteLine("Введите число 1");
+// int a= int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите число 2");
+// int b= int.Parse(Console.ReadLine());
+// if (a > b)
+// {
+//     Console.WriteLine("max " + a);
+// }
+// else if (b > a)
+// {
+//     Console.WriteLine("max " + b);
+// }
+// else
+// {
+//     Console.WriteLine("Значения ровны");
+// }
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.WriteLine("Задача 2");
+
+//         Console.WriteLine("Введите число 1");
+//         int a = int.Parse(Console.ReadLine());
+//         Console.WriteLine("Введите число 2");
+//         int b = int.Parse(Console.ReadLine());
+//         Console.WriteLine("Введите число 3");
+//         int c = int.Parse(Console.ReadLine());
+//         if (c < a && a > b)
+//         {
+//             Console.WriteLine("max " + a);
+//         }
+//         else if (c < b && b > a)
+//         {
+//             Console.WriteLine("max " + b);
+//         }
+//         else if (a < c && c > b)
+//         {
+//             Console.WriteLine("max " + c);
+//         }
+//     }
+// }
